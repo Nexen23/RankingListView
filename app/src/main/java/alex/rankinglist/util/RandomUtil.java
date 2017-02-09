16b65556f8f3e84db9663fc6f08c6a06efebebb3
Nexen23 <tsalexn@mail.ru>
2017-02-09 14:55:16 +0700

    added group breaking animation

--- app/src/main/java/alex/rankinglist/util/RandomUtil.java
+++ app/src/main/java/alex/rankinglist/util/RandomUtil.java
@@ -10,7 +10,7 @@ import java.util.Random;
 import alex.rankinglist.widget.model.User;
 
 public class RandomUtil {
-	private static boolean ENABLE_RANDOM = true;
+	private static boolean ENABLE_RANDOM = false;
 	private static Randomizer randomizer = new Randomizer();
 
 	public static String GenerateName() {
@@ -25,7 +25,7 @@ public class RandomUtil {
 	public static List<User> GenerateUsersList(@IntRange(from=0) int count) {
 		List<User> users = new ArrayList<>(count);
 		if (!ENABLE_RANDOM) {
-			for (int i = 0; i <= 6; ++i) {
+			for (int i = 0; i <= 10; ++i) {
 				users.add(new User(GenerateName(), i * 5));
 			}
 
--- app/src/main/java/alex/rankinglist/widget/UsersView.java
+++ app/src/main/java/alex/rankinglist/widget/UsersView.java
@@ -1,9 +1,12 @@
 package alex.rankinglist.widget;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.FrameLayout;
 
 import junit.framework.Assert;
@@ -23,8 +26,9 @@ import alex.rankinglist.widget.model.User;
 public class UsersView extends FrameLayout implements GroupedList.EventsListener {
 	private Rank rank;
 	private GroupedList groupedList;
-	private LinkedList<View> animationViews = new LinkedList<>();
+	private LinkedList<View> animatedViews = new LinkedList<>();
 	private HashMap<GroupNode, GroupView> groupsViews = new HashMap<>();
+	private int fadeDuration, moveDuration;
 
 	public UsersView(Context context) {
 		super(context);
@@ -42,6 +46,10 @@ public class UsersView extends FrameLayout implements GroupedList.EventsListener
 	}
 
 	void init() {
+		//fadeDuration = getResources().getInteger(R.integer.animation_fast_duration);
+		//moveDuration = getResources().getInteger(R.integer.animation_slow_duration);
+		fadeDuration = 150;
+		moveDuration = 300;
 		groupedList = new GroupedList(getResources().getDimensionPixelSize(R.dimen.group_view_height));
 		groupedList.addListener(this);
 	}
@@ -90,26 +98,132 @@ public class UsersView extends FrameLayout implements GroupedList.EventsListener
 
 	@Override
 	public void onBreak(GroupNode removedGroup, GroupNode a, GroupNode b) {
-		GroupView recycledView = groupsViews.remove(removedGroup);
-		groupsViews.put(a, recycledView);
+		final GroupView removedView = groupsViews.remove(removedGroup);
+		removedView.animate().cancel();
+		if (removedView.getTag() instanceof AnimationData) {
+			((AnimationData) removedView.getTag()).animator.cancel();
+		}
+		removedView.setTag(removedGroup);
+		animatedViews.add(removedView);
+		removedView.animate()
+				.alpha(0)
+				.setDuration(fadeDuration)
+				.setInterpolator(new LinearInterpolator())
+				.withEndAction(() -> {
+					removeView(removedView);
+					animatedViews.remove(removedView);
+				});
+
+		float initPos = removedGroup.getAbsolutePos(getHeight());
+
 
 		GroupView newView = new GroupView(getContext());
-		addView(newView);
+		addView(newView, 0);
+		groupsViews.put(a, newView);
+		setAnimation(newView, a, initPos);
+		setViewModel(newView, a);
+
+		newView = new GroupView(getContext());
+		addView(newView, 0);
 		groupsViews.put(b, newView);
+		setAnimation(newView, b, initPos);
+		setViewModel(newView, b);
+	}
+
+
+	void setAnimation(final View newView, final GroupNode a, float initPos) {
+		float initNormalizedPos = initPos / getHeight();
+		ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
+		newView.setTag(new AnimationData(animator, a, initNormalizedPos));
+
+		animator.setDuration(fadeDuration).setInterpolator(new LinearInterpolator());
+		animator.addUpdateListener(animation -> {
+			final AnimationData tag = (AnimationData) newView.getTag();
+			tag.setAnimatedValue(((float) animation.getAnimatedValue()));
+			tag.updateY(newView);
+		});
+		animator.addListener(new Animator.AnimatorListener() {
+			@Override
+			public void onAnimationStart(Animator animation) {
+
+			}
+
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				final AnimationData tag = (AnimationData) newView.getTag();
+				tag.setAnimatedValue(1);
+				tag.updateY(newView);
+				newView.setTag(null);
+			}
+
+			@Override
+			public void onAnimationCancel(Animator animation) {
+
+			}
+
+			@Override
+			public void onAnimationRepeat(Animator animation) {
+
+			}
+		});
+		animator.start();
 	}
 
 	private void updateGroupsViews() {
-		Assert.assertEquals(getChildCount(), groupedList.getGroupsCount());
+		Assert.assertEquals(getChildCount(), groupedList.getGroupsCount() + animatedViews.size());
 
 		for (GroupNode group : groupedList) {
 			GroupView view = groupsViews.get(group);
-			view.setY(group.getAbsolutePos(getHeight()));
-
-			if (group.isLeaf()) {
-				view.setModel(group.getData());
+			if (view.getTag() == null) {
+				view.setY(group.getAbsolutePos(getHeight()));
 			} else {
-				view.setModel(group.getData(), group.getItemsCount(), group.getAvgScore(rank));
+				if (view.getTag() instanceof AnimationData) {
+					((AnimationData) view.getTag()).updateY(view);
+				} else {
+					throw new IllegalStateException("all views with GroupNode in tag should be in animatedViews array");
+				}
 			}
+
+			setViewModel(view, group);
+		}
+
+		for (View view : animatedViews) {
+			final GroupNode group = (GroupNode) view.getTag();
+			view.setY(group.getAbsolutePos(getHeight()));
+		}
+	}
+
+	private void setViewModel(GroupView view, GroupNode group) {
+		if (group.isLeaf()) {
+			view.setModel(group.getData());
+		} else {
+			view.setModel(group.getData(), group.getItemsCount(), group.getAvgScore(rank));
+		}
+	}
+
+	class AnimationData {
+		public GroupNode group;
+		public float initNormalizedPos;
+		public Animator animator;
+		private float animatedValue;
+
+		public AnimationData(Animator animator, GroupNode group, float initNormalizedPos) {
+			this.animator = animator;
+			this.group = group;
+			this.initNormalizedPos = initNormalizedPos;
+		}
+
+		public void setAnimatedValue(float animatedValue) {
+			this.animatedValue = animatedValue;
+		}
+
+		public void updateY(View view) {
+			final float shouldBeAbsolutePos = group.getAbsolutePos(getHeight());
+			final float initAbsolutePos = getHeight() * initNormalizedPos;
+			final float distance = initAbsolutePos - shouldBeAbsolutePos;
+
+			view.setY(initAbsolutePos - distance * animatedValue);
+			//LogUtil.d(this, "ANIMATION:: %.2f", animatedValue);
 		}
 	}
 }
